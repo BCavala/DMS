c7e1fe73f9527cea69b8d94e51e15a5bda816823
BCavala <33980651+BCavala@users.noreply.github.com>
2024-06-07 14:14:03 +0200

    Update stapp.py

--- stapp.py
+++ stapp.py
@@ -1,11 +1,10 @@
-import av
 import cv2
 import mediapipe as mp
 import numpy as np
 import pickle
 import pandas as pd
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, WebRtcMode
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, RTCConfiguration
 
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
@@ -14,14 +13,11 @@ mp_face_mesh = mp.solutions.face_mesh
 with open('face_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-class VideoProcessor(VideoProcessorBase):
+class FaceProcessor(VideoTransformerBase):
     def __init__(self):
         self.face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-        self.yawn_counter = 0
-        self.sleeping_counter = 0
-        self.distracted_counter = 0
 
-    def recv(self, frame):
+    def transform(self, frame):
         image = frame.to_ndarray(format="bgr24")
 
         # Recolor image to RGB
@@ -50,32 +46,28 @@ class VideoProcessor(VideoProcessorBase):
                 # Display the results
                 if face_action_class == 'Yawn' and max(face_action_prob) > 0.75:
                     cv2.putText(image, 'Yawning Detected', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-                    self.yawn_counter += 1
                 if face_action_class == 'Sleeping' and max(face_action_prob) > 0.65:
                     cv2.putText(image, 'Sleeping Detected', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-                    self.sleeping_counter += 1
                 if face_action_class == 'Distracted' and max(face_action_prob) > 0.65:
                     cv2.putText(image, 'Distracted', (10, 110), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-                    self.distracted_counter += 1
                 if face_action_class == 'Neutral' and max(face_action_prob) > 0.85:
                     cv2.putText(image, 'Neutral', (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-        return av.VideoFrame.from_ndarray(image, format="bgr24")
+        return image
+
+rtc_configuration = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
 
 st.title("Sustav za nadzor vozača")
 
 webrtc_ctx = webrtc_streamer(
     key="example",
-    mode=WebRtcMode.SENDRECV,
-    video_processor_factory=VideoProcessor,
-    media_stream_constraints={"video": True, "audio": False},
-    async_processing=True,
+    video_transformer_factory=FaceProcessor,
+    rtc_configuration=rtc_configuration,
+    async_transform=True,
 )
 
-if webrtc_ctx.video_processor:
+if webrtc_ctx.video_transformer:
     if st.button("Stop"):
         webrtc_ctx.stop()
-
-    st.text(f"Yawns: {webrtc_ctx.video_processor.yawn_counter}")
-    st.text(f"Sleeping: {webrtc_ctx.video_processor.sleeping_counter}")
-    st.text(f"Distracted: {webrtc_ctx.video_processor.distracted_counter}")
