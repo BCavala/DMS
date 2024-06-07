66e7a4e4d7a4aed5e3e39fd2a03b02a56924d79f
BCavala <33980651+BCavala@users.noreply.github.com>
2024-06-07 14:32:25 +0200

    Update stapp.py

--- stapp.py
+++ stapp.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 import pandas as pd
 import streamlit as st
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, RTCConfiguration
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, RTCConfiguration, WebRtcMode
 
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
@@ -63,9 +63,11 @@ st.title("Sustav za nadzor vozača")
 
 webrtc_ctx = webrtc_streamer(
     key="example",
+    mode=WebRtcMode.SENDRECV,
     video_processor_factory=FaceProcessor,
     rtc_configuration=rtc_configuration,
     async_processing=True,  # Changed from async_transform to async_processing
+    media_stream_constraints={"video": True, "audio": False},
 )
 
 if webrtc_ctx.video_processor:
